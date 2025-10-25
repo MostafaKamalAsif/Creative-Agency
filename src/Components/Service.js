1a60485d81ab4dc7a111f98fd83eed1ba7ca5dcf
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-10-25 12:24:05 +0600

    service start

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Container from './Container'
-
+import Image from 'next/image'
+import UI from "/public/UI.svg"
 const Service = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const Service = () => {
           </h3>
           <p className='w-[542px] m-auto text-center pt-5 pb-12.5'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
             </div>
+            <Image src={UI}  alt='UI' />
           
         </Container>
     </div>
