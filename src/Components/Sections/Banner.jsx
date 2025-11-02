6ae43ceb1fef6fec5c9bf2c2656b83cb28fe0419
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-11-02 06:37:24 +0600

    ..

--- src/Components/Sections/Banner.jsx
+++ src/Components/Sections/Banner.jsx
@@ -10,8 +10,8 @@ const Banner = () => {
         <Container>
             <div className={" md:flex  md:items-center  md:justify-between"}>
 <div className='md:w-[49%] mx-auto md:mx-0 '>
-    <h1 className='text-[35px] lg:text-[65px] text-white  font-bold px-20 md:px-0 text-center md:text-start lg:w-[501px]  lg:leading-20'>We Are Digital Product Design Agency</h1>
-    <p className='text-[14px] md:text-[16px] text-center md:text-start leading-5 md:leading-[27px] text-white  lg:w-[542px] pt-[25px] pb-7.5'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+    <h1 className='text-[35px] lg:text-[65px] text-white  font-bold sm:w-[300px] mx-auto md:mx-0 md:px-0 text-center md:text-start lg:w-[501px]  lg:leading-20'>We Are Digital Product Design Agency</h1>
+    <p className='text-[14px] md:text-[16px] text-center md:text-start leading-5 md:leading-[27px] text-white px-7.5 md:px-0  lg:w-[542px] pt-[25px] pb-7.5'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
     <Button className={"bg-white mx-auto md:mx-0 mb-5 md:mb-0 text-[#5956E8] w-fit py-3 px-6  rounded-[10px]  text-[14px] md:text-[18px] font-medium "} href={"/contact"} text={"Get Started"}/>
 </div>
 <div className="md:w-[49%]">
